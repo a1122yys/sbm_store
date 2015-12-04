fc636072e288a7585f4b8a7883b94e5a8d7c0e9a
a1122yys <895091836@qq.com>
2015-12-04 11:51:32 +0800

    Initial commit

---
+++ app/src/main/java/aizhinong/yys/spamao/MainActivity.java
@@ -0,0 +1,16 @@
+package aizhinong.yys.spamao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
